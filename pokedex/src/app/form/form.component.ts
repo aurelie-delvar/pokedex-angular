7baf107beace6b303ee074ee69a3c8986726356f
Aurélie DELVAR <adelvar@norsys.fr>
2024-07-02 14:49:56 +0200

    Champ file sur form

--- pokedex/src/app/form/form.component.ts
+++ pokedex/src/app/form/form.component.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { ButtonComponent } from '../button/button.component';
+import { HttpClient } from '@angular/common/http';
+import { throwError } from 'rxjs';
 
 @Component({
   selector: 'app-form',
@@ -11,8 +13,38 @@ import { ButtonComponent } from '../button/button.component';
   styleUrl: './form.component.css'
 })
 export class FormComponent {
+
   applyForm = new FormGroup({
     pokemonName: new FormControl(''),
     authorName: new FormControl('')
-  })
+  });
+  file: File | null = null;
+  
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void { }
+  
+  onChange(event: any) {
+    const file: File = event.target.files[0];
+
+    if (file) {
+      this.file = file;
+    }
+  }
+
+  onUpload() {
+    if (this.file) {
+      const formData = new FormData();
+
+      formData.append("file", this.file, this.file.name);
+
+      const upload$ = this.http.post("https://httpbin.org/post", formData);
+
+      upload$.subscribe({
+        error: (error: any) => {
+          return throwError(() => error);
+        }
+      })
+    }
+  }
 }
